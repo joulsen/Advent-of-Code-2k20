c01658fc96b49f91e54d524493a57f49f3d0ef8a
Andreas J. P <supercigar@gmail.com>
2020-12-05 15:58:16 +0100

    Added day 2

---
+++ day02.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 15:37:53 2020
+
+@author: Andreas J. P.
+"""
+
+import re
+parser = re.compile(r"(\d+)-(\d+) (\w): (\w+)")
+
+with open("input/day2.txt") as file:
+    database = parser.findall(file.read())
+
+
+def part1():
+    return sum([1 for lower, upper, char, pw in database
+                if int(lower) <= pw.count(char) <= int(upper)])
+
+
+def part2():
+    return sum([1 for first, second, char, pw in database
+                if (pw[int(first) - 1] == char) ^  # XOR
+                (pw[int(second) - 1] == char)])
+
+
+# Printing results
+print("Answer to AoC day 2 part 1 is: {}".format(part1()))
+print("Answer to AoC day 2 part 2 is: {}".format(part2()))
